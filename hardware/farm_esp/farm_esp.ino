7ed8f13459521097d69356c9c1ae5bcd58e63df7
Adrivid <adrividmishra@gmail.com>
2026-02-13 15:33:35 +0530

    Before review-1

---
+++ hardware/farm_esp/farm_esp.ino
@@ -0,0 +1,40 @@
+#include <DHT.h>
+
+//DHT
+constexpr uint8_t PIN_DHT = 4;
+#define DHTTYPE DHT22
+DHT dht(PIN_DHT, DHTTYPE);
+
+//LDR
+#define LIGHT_SENSOR_PIN 34
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  analogSetAttenuation(ADC_11db); //for LDR
+}
+
+void loop() {
+  // float air_temperature = dht.readTemperature();
+  // float air_humidity = dht.readHumidity();
+
+  // if (isnan(air_temperature) || isnan(air_humidity)) {
+  //   Serial.println("DHT read failed");
+  // } else {
+  //   Serial.print("Temp: ");
+  //   Serial.print(air_temperature);
+  //   Serial.print(" °C  | Humidity: ");
+  //   Serial.print(air_humidity);
+  //   Serial.println(" %");
+  // }
+
+
+  //LDR
+  float lux = (analogRead(LIGHT_SENSOR_PIN) / 4095.0) * 15000.0;
+  // Serial.println(lux);
+  
+
+
+
+  delay(2000);  
+}
